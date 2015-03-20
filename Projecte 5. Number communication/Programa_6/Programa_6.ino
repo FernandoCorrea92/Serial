6dfea8f74828bb98e7f160a03ee7a40104575c31
FernandoCorrea92 <fernando92correa@gmail.com>
2015-03-20 20:57:42 +0100

    programa 6 finalitzat

--- Projecte 5. Number communication/Programa_6/Programa_6.ino
+++ Projecte 5. Number communication/Programa_6/Programa_6.ino
@@ -10,8 +10,9 @@
 //******  Variables  *****************************************************
 int numero;
 int nnum;
-int suma;
- 
+long suma;
+float mitja;
+
 //******  Setup  *********************************************************
 void setup () {
   Serial.begin(9600);
@@ -31,25 +32,29 @@ void loop () {
      Serial.print(" - ");
      
      if (Serial.read() == '\n') {
-       nnum=nnum+1;
-       suma=suma+numero;
+       nnum++;
+       suma = suma + numero;
      }
     }
-  }while(numero!=0)
-  
-     Serial.print("S'han entrat ");
-     Serial.print(nnum);
-     Serial.print(" numeros, ");
-     Serial.println("la suma de tots ells es ");
-     Serial.print(suma);
-     Serial.println("i la mitja ");
-     Serial.print(suma/nnum);
-  
-
+  }while(numero!=0);
  
-    
-     Serial.println();
-     Serial.println("Entrar numeros un a un (per acabar 0:");
+   nnum--;
+   mitja = float(suma)/nnum;
   
+   Serial.println("");
+   Serial.print("S'han entrat ");
+   Serial.print(nnum);
+   Serial.print(" numeros, ");
+   Serial.print("la suma de tots ells es ");
+   Serial.print(suma);
+   Serial.print(" i la mitja ");
+   Serial.print(mitja);
+   Serial.println(".");
+   Serial.println();
+   Serial.println("Entrar numeros un a un (per acabar 0):");
+   nnum = 0;
+   numero = 1;
+   suma = 0;
+   mitja = 0;
 }
   
